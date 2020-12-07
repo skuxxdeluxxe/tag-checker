2e1231acc961c2c365417ae65cd9928b195d8631
Shantanu Padhye <shantanu.padhye@myob.com>
2020-12-07 19:13:17 +1300

    Remove functionality i do not need

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,11 +1,5 @@
 const tagCheckerService = require('./../services/tagchecker');
 
-const saySomething = (req, res, next) => {
-    res.status(200).json({
-        body: 'Hello from the server!'
-    });
-};
-
 const tagChecker = (req, res, next) => {
     var result = tagCheckerService.tagChecker(req.body)
     res.status(200).json({
--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('./../controllers/controllers');
 
-router.get('/say-something', controllers.saySomething);
-
-router.get('/tag-checker', controllers.tagChecker);
-
 router.post('/tag-checker', controllers.tagChecker);
 
 module.exports = router;
